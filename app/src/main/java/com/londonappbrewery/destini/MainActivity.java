3a738b552a47fbb169d02e9412eee2fa277c2cdb
Stefan Starkbaum <starkbaum.stefan@gmail.com>
2019-01-19 12:28:39 +0100

    Initial commit

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,29 +2,66 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
-
+    private Button mButtonTop;
+    private Button mButtonBottom;
+    private TextView mTextView;
+    private int mIndex = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        mButtonTop = findViewById(R.id.buttonTop);
+        mButtonBottom = findViewById(R.id.buttonBottom);
+        mTextView = findViewById(R.id.storyTextView);
 
+        mButtonTop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-        // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
+                if (mIndex == 1 || mIndex == 2) {
+                    setText(R.string.T3_Story, R.string.T3_Ans1, R.string.T3_Ans2);
+                    mIndex = 3;
+                } else if (mIndex == 3) {
+                    mTextView.setText(R.string.T6_End);
+                    hideButtons();
+                }
+            }
+        });
 
+        mButtonBottom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mIndex == 1) {
+                    setText(R.string.T2_Story, R.string.T2_Ans1, R.string.T2_Ans2);
+                    mIndex = 2;
+                } else if (mIndex == 2) {
+                    mTextView.setText(R.string.T4_End);
+                    hideButtons();
+                } else if (mIndex == 3) {
+                    mTextView.setText(R.string.T5_End);
+                    hideButtons();
+                }
+            }
+        });
+    }
 
+    private void setText(int textView, int buttonTop, int buttonBottom) {
+        mTextView.setText(textView);
+        mButtonTop.setText(buttonTop);
+        mButtonBottom.setText(buttonBottom);
+    }
 
-        // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
-
-
-
-
-        // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
-
-
+    private void hideButtons() {
+        mButtonTop.setVisibility(View.GONE);
+        mButtonBottom.setVisibility(View.GONE);
     }
 }
